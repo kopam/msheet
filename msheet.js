9b47422ce264ce38bfaefffdba2ba9ed73b6bdd0
Petar Vlahu <vlahupetar@gmail.com>
2014-08-30 20:26:02 -0400

    expression parser; initial commit

--- msheet.js
+++ msheet.js
@@ -38,7 +38,7 @@ if (Meteor.isClient) {
   });
   
   Template.sheet.events({
-    'keypress .cell': function(e){
+    'keyup .cell': function(e){
       var ct = e.currentTarget;
       var col = this.col;
       var row = this.row;
@@ -52,14 +52,34 @@ if (Meteor.isClient) {
       }
       if (e.which === 13 || e.which === 9){
         document.querySelector('.row:nth-child('+row+') .cell:nth-child('+col+')').focus();
+        e.preventDefault();
+      }
+      if (e.which === 27){
+        ct.blur();
+      }
+    },
+    'focus .cell':function(e){
+      if (this.expr){
+        e.currentTarget.value = this.expr;
       }
     },
     'blur .cell':function(e){
-      Cells.update({_id:this._id}, {
+      var expression = e.currentTarget.value;
+      var val = expression;
+      if (expression.indexOf("=") === 0){
+         val = Parser.parse(expression.substring(1));
+      }
+      var c = {
         row:this.row,
         col:this.col,
-        val:e.currentTarget.value
-      });
+        val:val
+      };
+      if (expression.indexOf("=") === 0)
+        c.expr = expression;
+      
+      Cells.update({_id:this._id}, c);
+      
+      e.currentTarget.value = val;
     }
   });
 
@@ -70,5 +90,17 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    var cells = Cells.find({});
+    if (cells.count() === 0){
+      for (var i = 1; i <= cols; i++) {
+        for (var j = 1; j <= cols; j++) {
+          Cells.insert({
+            row: i,
+            col: j,
+            val: ''
+          });
+        }
+      }
+    }
   });
 }
